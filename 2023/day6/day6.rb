7e65e5b1cbec77ed7ae27ef0b3330348d25989b0
Andy Mikula <andy@andymikula.ca>
2023-12-05 22:15:45 -0700

    Day 6

--- 2023/day6/day6.rb
+++ 2023/day6/day6.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-input = File.readlines('input.txt')
+puts "We're looking for the domain of the inequality `hold(time - hold) >= distance`, where `hold` is the time you hold the button, and `distance` is the distance you need to beat. I just used Wolfram Alpha to solve this :)"
